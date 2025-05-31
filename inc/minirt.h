373d7b9bc8ee94f99776fe69db9fc91971961532
Sorawit Klaokliang <sklaokli@z3t6c2.42bangkok.com>
2025-05-31 19:26:36 +0700

    header

--- inc/minirt.h
+++ inc/minirt.h
@@ -6,7 +6,7 @@
 /*   By: sklaokli <sklaokli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 13:17:12 by sklaokli          #+#    #+#             */
-/*   Updated: 2025/05/31 18:56:35 by sklaokli         ###   ########.fr       */
+/*   Updated: 2025/05/31 19:26:01 by sklaokli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,34 @@
 # include "MLX42/MLX42.h"
 # include "MLX42/MLX42_Int.h"
 
+typedef enum s_identifier
+{
+	AMBIENT_LIGHTNING,
+	CAMERA,
+	LIGHT,
+	SPHERE,
+	PLANE,
+	CYLINDER
+}	t_identifier;
+
+typedef struct s_vector
+{
+	float	x;
+	float	y;
+	float	z;
+}	t_vector;
+
+typedef struct s_color
+{
+	size_t	r;
+	size_t	g;
+	size_t	b;
+}	t_color;
+
 typedef struct s_rt
 {
-	void	*ptr;
+	t_identifier	idtf;
+	t_vector		axis;
 }	t_rt;
 
 
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sklaokli <sklaokli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 14:07:09 by sklaokli          #+#    #+#             */
-/*   Updated: 2025/05/31 18:57:49 by sklaokli         ###   ########.fr       */
+/*   Updated: 2025/05/31 19:25:56 by sklaokli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,5 @@
 
 int	main(void)
 {
-	t_rt	*rt;
-
-	rt = gct_malloc(sizeof(t_rt));
-	gct_malloc(100000000);
-	gct_malloc(100000000);
-	gct_malloc(100000000);
-	gct_malloc(100000000);
-	gct_malloc(100000000);
-	gct_malloc(100000000);
-	gct_malloc(100000000);
-	gct_malloc(100000000);
-	gct_malloc(100000000);
-	gct_malloc(100000000);
-	gct_malloc(100000000);
-	gct_malloc(100000000);
-	gct_malloc(100000000);
-	gct_malloc(10000000000000000);
-	gct_malloc(100000000);
-	gct_malloc(100000000);
-	gct_malloc(100000000);
-	gct_malloc(100000000);
-	gct_malloc(100000000);
-	gct_malloc(100000000);
-	printf("%p\n", &rt);
-	gct_cleanup();
-	printf("dawd");
 	return (0);
 }
