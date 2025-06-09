c13b3fb28c9d4a7d52417ea76466ce17fb36bb24
Sorawit Klaokliang <sklaokli@z3t8c3.42bangkok.com>
2025-06-09 21:49:54 +0700

    header

--- inc/minirt.h
+++ inc/minirt.h
@@ -6,7 +6,7 @@
 /*   By: sklaokli <sklaokli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 16:45:36 by sklaokli          #+#    #+#             */
-/*   Updated: 2025/06/09 20:39:15 by sklaokli         ###   ########.fr       */
+/*   Updated: 2025/06/09 21:49:41 by sklaokli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,22 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdint.h>
 # include <stdio.h>
 # include <math.h>
 
 # include "libft.h"
 # include "MLX42/MLX42.h"
 # include "MLX42/MLX42_Int.h"
+
+# include "object.h"
+
+typedef struct s_rt
+{
+	t_ambient	ambient;
+	t_camera	camera;
+	t_light		light;
+	t_object	*object;
+}	t_rt;
 
 #endif
---
+++ inc/object.h
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   object.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sklaokli <sklaokli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/09 21:49:17 by sklaokli          #+#    #+#             */
+/*   Updated: 2025/06/09 21:49:23 by sklaokli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef OBJECT_H
+# define OBJECT_H
+
+# include <unistd.h>
+# include <stdint.h>
+
+typedef struct s_rgb
+{
+	uint8_t	r;
+	uint8_t	g;
+	uint8_t	b;
+}	t_rgb;
+
+typedef struct s_vector
+{
+	float	x;
+	float	y;
+	float	z;
+}	t_vector;
+
+typedef enum s_idtf
+{
+	UNRECOGNIZED,
+	AMBIENT,
+	CAMERA,
+	LIGHT,
+	SPHERE,
+	PLANE,
+	CYLINDER
+}	t_idtf;
+
+typedef struct s_ambient
+{
+	t_idtf		idtf;
+	float		ratio;
+	t_rgb		color;
+}	t_ambient;
+
+typedef struct s_camera
+{
+	t_idtf		idtf;
+	t_vector	pos;
+	t_vector	dir;
+	size_t		fov;
+}	t_camera;
+
+typedef struct s_object
+{
+	struct s_object		*next;
+	t_idtf				idtf;
+}	t_object;
+
+typedef struct s_light
+{
+	struct s_light		*next;
+	t_idtf				idtf;
+	t_vector			pos;
+	float				brightness;
+	t_rgb				color;
+}	t_light;
+
+typedef struct s_sphere
+{
+	struct s_sphere		*next;
+	t_idtf				idtf;
+	t_vector			pos;
+	float				diameter;
+}	t_sphere;
+
+typedef struct s_plane
+{
+	struct s_plane		*next;
+	t_idtf				idtf;
+}	t_plane;
+
+typedef struct s_cylinder
+{
+	struct s_cylinder	*next;
+	t_idtf				idtf;
+}	t_cylinder;
+
+#endif
